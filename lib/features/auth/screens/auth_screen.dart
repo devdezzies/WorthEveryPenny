acea8d6ecf06b4f8147712622208f259b7aa37cd
Abdullah <boltholt17@gmail.com>
2024-11-25 15:02:42 +0700

    feat: double container auth

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -18,9 +18,12 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   final _signUpFormKey = GlobalKey<FormState>();
+  final _signInFormKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
+
+  final Auth _auth = Auth.signup;
 
   @override
   void dispose() {
@@ -42,27 +45,44 @@ class _AuthScreenState extends State<AuthScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text(
-                "The Place where Your Stuff is Valuable!",
+                "Know Your Money in One Look",
                 style: TextStyle(
                     color: GlobalVariables.secondaryColor,
                     fontSize: 22,
                     fontWeight: FontWeight.bold),
               ),
+              // TODO: Toggle Switch for authentication page
+              // ToggleButtons(children: , isSelected: isSelected)
               const SizedBox(height: 25,),
-              Form(
-                key: _signUpFormKey,
-                child: Column(
-                  children: [
-                    CustomTextfield(controller: _nameController, hintText: "Your Name",), 
-                    const SizedBox(height: 18,),
-                    CustomTextfield(controller: _emailController, hintText: "Your Email",), 
-                    const SizedBox(height: 18,),
-                    CustomTextfield(controller: _passwordController, hintText: "Your Password",), 
-                    const SizedBox(height: 18,), 
-                    CustomButton(textTitle: "Sign Up", onTap: () {})
-                  ],
+              if (_auth == Auth.signup)
+                Form(
+                  key: _signUpFormKey,
+                  child: Column(
+                    children: [
+                      CustomTextfield(controller: _nameController, hintText: "Your Name",), 
+                      const SizedBox(height: 18,),
+                      CustomTextfield(controller: _emailController, hintText: "Your Email",), 
+                      const SizedBox(height: 18,),
+                      CustomTextfield(controller: _passwordController, hintText: "Your Password",), 
+                      const SizedBox(height: 18,), 
+                      CustomButton(textTitle: "Sign Up", onTap: () {}),
+                    ],
+                  ),
                 ),
-              )
+              
+              if (_auth == Auth.signin)
+                Form(
+                  key: _signInFormKey,
+                  child: Column(
+                    children: [
+                      CustomTextfield(controller: _emailController, hintText: "Your Email",), 
+                      const SizedBox(height: 18,),
+                      CustomTextfield(controller: _passwordController, hintText: "Your Password",), 
+                      const SizedBox(height: 18,), 
+                      CustomButton(textTitle: "Login", onTap: () {}),
+                    ],
+                  ),
+                )
             ],
           ),
         ),
