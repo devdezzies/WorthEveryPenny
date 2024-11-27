329c6ddc46c48e7694a51e8e82cad9299a8bb872
Abdullah <boltholt17@gmail.com>
2024-11-27 20:03:25 +0700

    feat: add userScheme and POST middleware for user

--- server/index.js
+++ server/index.js
@@ -8,13 +8,18 @@ const authRouter = require('./routes/auth');
 // INIT
 const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://boltholt17:Npriwnl39zXZNaX3@cluster0.0x0x9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-// // CONNECTIONS
-// mongoose.connect().then(() => {
-//     console.log("Connection successfull");
-// }).catch((e) => {
-//     console.log(e);
-// })
+// MIDDLEWARE 
+app.use(express.json())
+app.use(authRouter)
+
+// CONNECTIONS
+mongoose.connect(DB).then(() => {
+    console.log("Connection successfull");
+}).catch((e) => {
+    console.log(e);
+})
 
 app.get("/api/user", (req, res) => {
     res.json({name: 'hello'})
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,32 @@
-const express = require('express'); 
+const express = require('express')
+const User = require("../models/user")
+const bcryptjs = require('bcryptjs')
 
 const authRouter = express.Router();
 
-// authRouter.get("/user", (req, res) => {
-//     res.json({
-//         msg: "hello"
-//     })
-// })
-
-authRouter.post('api/signup', (req, res) => {
-    // get the data from the client
-    console.log(req.body);
-    // post that data in database
+authRouter.post('/api/signup', async (req, res) => {
+    try {
+        const {name, email, password} = req.body
     
+        const exsitingUser = await User.findOne({ email })
+        if (exsitingUser) { // Equals to != NULL
+            return res.status(400).json({msg: 'User with the same email already exists!'})
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 8)
+    
+        let user = new User({
+            name, 
+            email,
+            password : hashedPassword,
+        })
+        user = await user.save()
+        res.json(user)
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+    
+    // post that data in database
     // return that data to the user 
 })
 
