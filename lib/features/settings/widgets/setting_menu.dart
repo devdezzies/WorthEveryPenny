47e0e7407bd724b7cccbd16828af5efb741e364e
devdezzies <boltholt17@gmail.com>
2025-01-15 19:46:11 +0700

    feat: enhance SettingMenu with preview titles and update spacing in settings screen

--- lib/features/settings/widgets/setting_menu.dart
+++ lib/features/settings/widgets/setting_menu.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:swappp/constants/global_variables.dart';
 
 class SettingMenu extends StatelessWidget {
   final IconData headIcon; 
   final String menuTitle;
+  final String? previewTitle;
   final void Function() onTap;
-  const SettingMenu({super.key, required this.headIcon, required this.menuTitle, required this.onTap});
+  const SettingMenu({super.key, required this.headIcon, required this.menuTitle, required this.onTap, this.previewTitle});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      
       onTap: onTap,
       child: Row(
         children: [
@@ -17,6 +20,15 @@ class SettingMenu extends StatelessWidget {
             child: Icon(headIcon, size: 30),
           ), 
           Expanded(child: Text(menuTitle, style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 16))), 
+          if (previewTitle != null) 
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: GlobalVariables.secondaryColor,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Text(previewTitle ?? "", style: const TextStyle(fontWeight: FontWeight.w700, color: GlobalVariables.backgroundColor)),
+            ),
           const Icon(Icons.keyboard_arrow_right, size: 30,)
         ],
       ),
